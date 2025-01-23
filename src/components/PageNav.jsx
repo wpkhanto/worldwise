12379006e948f5362ff35e727df9bdb21622edab
Watcharaphon Khanto <wpkhanto@icloud.com>
2025-01-23 20:34:05 +0700

    Using CSS Modules

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,18 +1,25 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from 'react-router-dom';
+import styles from './PageNav.module.css';
 
 function PageNav() {
     return (
-        <nav>
+        <nav className={styles.nav}>
             <ul>
                 {/* <li><Link to="/">Home</Link></li>
                 <li><Link to="/pricing">Pricing</Link></li>
                 <li><Link to="/product">Product</Link></li> */}
-                <li><NavLink to="/">Home</NavLink></li>
-                <li><NavLink to="/pricing">Pricing</NavLink></li>
-                <li><NavLink to="/product">Product</NavLink></li>
+                <li>
+                    <NavLink to='/'>Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/pricing'>Pricing</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/product'>Product</NavLink>
+                </li>
             </ul>
         </nav>
-    )
+    );
 }
 
-export default PageNav
+export default PageNav;
