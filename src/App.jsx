4f52ce71480c2916ad268b32064f4f1b274b6b86
Watcharaphon Khanto <wpkhanto@icloud.com>
2025-01-29 21:59:51 +0700

    Dynamic Routes With URL Parameters

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import AppLayout from './pages/AppLayout';
 import PageNotFound from './pages/PageNotFound';
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
+import City from './components/City';
 
 const BASE_URL = 'http://localhost:9000';
 
@@ -51,6 +52,7 @@ function App() {
                             <CityList cities={cities} isLoading={isLoading} />
                         }
                     />
+                    <Route path='cities/:id' element={<City />} />
                     <Route
                         path='countries'
                         element={
