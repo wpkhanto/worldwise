33eac65c54b18696065cacf399b49b2917450787
Watcharaphon Khanto <wpkhanto@icloud.com>
2025-01-30 21:52:08 +0700

    Reading and Setting a Query String

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from 'react-router-dom';
 // import styles from "./City.module.css";
 
 // const formatDate = (date) =>
@@ -10,56 +10,67 @@ import { useParams } from "react-router-dom";
 //   }).format(new Date(date));
 
 function City() {
-  const { id } = useParams();
-  // TEMP DATA
-  // const currentCity = {
-  //   cityName: "Lisbon",
-  //   emoji: "🇵🇹",
-  //   date: "2027-10-31T15:59:59.138Z",
-  //   notes: "My favorite city so far!",
-  // };
+    const { id } = useParams();
+    const [searchParams] = useSearchParams();
 
-  // const { cityName, emoji, date, notes } = currentCity;
+    const lat = searchParams.get('lat');
+    const lng = searchParams.get('lng');
+    // TEMP DATA
+    // const currentCity = {
+    //   cityName: "Lisbon",
+    //   emoji: "🇵🇹",
+    //   date: "2027-10-31T15:59:59.138Z",
+    //   notes: "My favorite city so far!",
+    // };
 
-  return <h1>City {id}</h1>
+    // const { cityName, emoji, date, notes } = currentCity;
 
-  // return (
-  //   <div className={styles.city}>
-  //     <div className={styles.row}>
-  //       <h6>City name</h6>
-  //       <h3>
-  //         <span>{emoji}</span> {cityName}
-  //       </h3>
-  //     </div>
+    return (
+        <>
+            <h1>City {id}</h1>;
+            <p>
+                Position: {lat}, {lng}
+            </p>
+        </>
+    );
 
-  //     <div className={styles.row}>
-  //       <h6>You went to {cityName} on</h6>
-  //       <p>{formatDate(date || null)}</p>
-  //     </div>
+    // return (
+    //   <div className={styles.city}>
+    //     <div className={styles.row}>
+    //       <h6>City name</h6>
+    //       <h3>
+    //         <span>{emoji}</span> {cityName}
+    //       </h3>
+    //     </div>
 
-  //     {notes && (
-  //       <div className={styles.row}>
-  //         <h6>Your notes</h6>
-  //         <p>{notes}</p>
-  //       </div>
-  //     )}
+    //     <div className={styles.row}>
+    //       <h6>You went to {cityName} on</h6>
+    //       <p>{formatDate(date || null)}</p>
+    //     </div>
 
-  //     <div className={styles.row}>
-  //       <h6>Learn more</h6>
-  //       <a
-  //         href={`https://en.wikipedia.org/wiki/${cityName}`}
-  //         target="_blank"
-  //         rel="noreferrer"
-  //       >
-  //         Check out {cityName} on Wikipedia &rarr;
-  //       </a>
-  //     </div>
+    //     {notes && (
+    //       <div className={styles.row}>
+    //         <h6>Your notes</h6>
+    //         <p>{notes}</p>
+    //       </div>
+    //     )}
 
-  //     <div>
-  //       <ButtonBack />
-  //     </div>
-  //   </div>
-  // );
+    //     <div className={styles.row}>
+    //       <h6>Learn more</h6>
+    //       <a
+    //         href={`https://en.wikipedia.org/wiki/${cityName}`}
+    //         target="_blank"
+    //         rel="noreferrer"
+    //       >
+    //         Check out {cityName} on Wikipedia &rarr;
+    //       </a>
+    //     </div>
+
+    //     <div>
+    //       <ButtonBack />
+    //     </div>
+    //   </div>
+    // );
 }
 
 export default City;
