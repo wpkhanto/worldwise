dd25290c94e6dc01ef0a09ef3500a6fc5746076f
wpkhanto <wpkhanto@icloud.com>
2025-01-31 13:45:22 +0700

    Programmatic Navigation with useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,65 @@
-import { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Product from './pages/Product';
-import Homepage from './pages/Homepage';
-import Pricing from './pages/Pricing';
-import Login from './pages/Login';
-import AppLayout from './pages/AppLayout';
-import PageNotFound from './pages/PageNotFound';
-import CityList from './components/CityList';
-import CountryList from './components/CountryList';
-import City from './components/City';
+import { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
+import PageNotFound from "./pages/PageNotFound";
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
 
-const BASE_URL = 'http://localhost:9000';
+const BASE_URL = "http://localhost:9000";
 
 function App() {
-    const [cities, setCities] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-        async function fetchCities() {
-            try {
-                setIsLoading(true);
-                const res = await fetch(`${BASE_URL}/cities`);
-                const data = await res.json();
-                setCities(data);
-            } catch (error) {
-                alert('error');
-            } finally {
-                setIsLoading(false);
-            }
-        }
-        fetchCities();
-    }, []);
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        alert("error");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
 
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<Homepage />} />
-                <Route path='product' element={<Product />} />
-                <Route path='pricing' element={<Pricing />} />
-                <Route path='login' element={<Login />} />
-                <Route path='app' element={<AppLayout />}>
-                    <Route
-                        index
-                        element={
-                            <CityList cities={cities} isLoading={isLoading} />
-                        }
-                    />
-                    <Route
-                        path='cities'
-                        element={
-                            <CityList cities={cities} isLoading={isLoading} />
-                        }
-                    />
-                    <Route path='cities/:id' element={<City />} />
-                    <Route
-                        path='countries'
-                        element={
-                            <CountryList
-                                cities={cities}
-                                isLoading={isLoading}
-                            />
-                        }
-                    />
-                    <Route path='form' element={<p>Form</p>} />
-                </Route>
-                <Route path='*' element={<PageNotFound />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="cities/:id" element={<City />} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="form" element={<Form />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,22 +1,24 @@
-import { useSearchParams } from 'react-router-dom';
-import styles from './Map.module.css';
+import { useNavigate, useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
 
 function Map() {
-    const [searchParams, setSearchParams] = useSearchParams();
-    const lat = searchParams.get('lat');
-    const lng = searchParams.get('lng');
+  const navigate = useNavigate();
 
-    return (
-        <div className={styles.mapContainer}>
-            <h1>Map</h1>;
-            <h1>
-                Position: {lat}, {lng}
-            </h1>
-            <button onClick={() => setSearchParams({ lat: 23, lng: 50 })}>
-                Change pos
-            </button>
-        </div>
-    );
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <h1>Map</h1>;
+      <h1>
+        Position: {lat}, {lng}
+      </h1>
+      <button onClick={() => setSearchParams({ lat: 23, lng: 50 })}>
+        Change pos
+      </button>
+    </div>
+  );
 }
 
 export default Map;
