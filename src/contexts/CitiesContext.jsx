f737f578fda150d568d2ceb21c5530a9aa031917
Watcharaphon Khanto <wpkhanto@icloud.com>
2025-02-07 23:53:01 +0700

    Finishing the City View 1/2

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -4,6 +4,7 @@ const CitiesContext = createContext();
 function CitiesProvider({ children }) {
     const [cities, setCities] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [currentCity, setCurrentCity] = useState({});
 
     useEffect(() => {
         async function fetchCities() {
@@ -21,8 +22,21 @@ function CitiesProvider({ children }) {
         fetchCities();
     }, []);
 
+    async function getCity(id) {
+        try {
+            setIsLoading(true);
+            const res = await fetch(`${BASE_URL}/cities/${id}`);
+            const data = await res.json();
+            setCurrentCity(data);
+        } catch (error) {
+            alert('error');
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
     return (
-        <CitiesContext.Provider value={{ cities, isLoading }}>
+        <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
             {children}
         </CitiesContext.Provider>
     );
